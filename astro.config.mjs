178dc2df17a077fd01fd15a4e152fc8c3a46eedb
wwwaaa123122 <wwwaaa123122@outlook.com>
2025-08-01 16:23:19 +0800

    91.top to blog.mcxclr.top

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://91sssvip.top",
+  site: "https://blog.mcxclr.top/",
   base: "/",
   trailingSlash: "always",
 
