2603e75f2830e7b4a724d03283e5834eb8c608e1
wwwaaa123122 <wwwaaa123122@outlook.com>
2025-08-04 13:24:49 +0800

    新增comments

---
+++ scripts/comments.js
@@ -0,0 +1,16 @@
+<script src="https://giscus.app/client.js"
+        data-repo="wwwaaa123122/blogcomment"
+        data-repo-id="R_kgDOPX1knA"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOPX1knM4CtwEh"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="zh-CN"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
