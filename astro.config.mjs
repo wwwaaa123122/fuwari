a0ff00d94f1a4fa8e86f7ca8bdeebb8891a93c98
wwwaaa123122 <wwwaaa123122@outlook.com>
2025-08-17 23:33:34 +0800

    增加置顶

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,6 +52,7 @@ export default defineConfig({
       }),
       icon({
           include: {
+            mdi: ["*"],
               "preprocess: vitePreprocess(),": ["*"],
               "fa6-brands": ["*"],
               "fa6-regular": ["*"],
