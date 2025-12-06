dffbed6cd6f81dfcba8d4a819689d526a8eba6f8
星辰旅人 <wwwaaa123122@outlook.com>
2025-12-06 11:34:44 +0000

    修改样式

--- src/config.ts
+++ src/config.ts
@@ -18,12 +18,12 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-    		src: "https://fpic.mcxclr.top", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    		src: "https://eopfapi.2b2x.cn/pic?img=ua", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "二叉树树的随机图", // Credit text to be displayed
+			url: "https://eopfapi.2b2x.cn/pic?img=ua", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
