65521d0343a786c8bd26a6fc9fd87adfdbfba822
wwwaaa123122 <wwwaaa123122@outlook.com>
2026-03-22 14:24:27 +0800

    png改为webg

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "./assets/homeground.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "./assets/homeground.webg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
